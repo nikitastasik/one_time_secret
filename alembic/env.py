a70fc93f14cac6a4818867644687a858c2b4b382
nikitas <nikita.s@foxcom.lv>
2024-08-06 14:27:06 +0300

    fix: fix app logic

--- alembic/env.py
+++ alembic/env.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
-from models import Base
+from app.models import Base
 
 # Загрузка переменных окружения из файла .env
 load_dotenv()
--- app/database.py
+++ app/database.py
@@ -14,7 +14,7 @@ db_name = os.getenv("DATABASE_NAME")
 
 # Формирование строки подключения
 DATABASE_URL = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
-
+print(DATABASE_URL)
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
